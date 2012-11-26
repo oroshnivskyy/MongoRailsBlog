3af0e44e15997583abc5dc5ff892c038634116d4
oroshnivskyy <oroshnivskyy@cogniance.com>
2012-11-26 17:52:13 +0200

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongo::Application.config.secret_token = '2f9c92757aece5c8fff9151b3ef81f7d8893e2127dbb473440f9d57df68713c72b692d6132c2f5c1d80cd07436ee6ee27dd9ab4d396d02dedc4cb6fd0a2d0abd'
